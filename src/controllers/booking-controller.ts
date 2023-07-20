e6ab70f912bc37080a7a032f3eba00afe051e75f
joseedivandro <jose.edivandro.ja@outlook.com>
2023-07-20 20:20:12 -0300

    fix(error): corrigindo mensagens de erros

--- src/controllers/booking-controller.ts
+++ src/controllers/booking-controller.ts
@@ -1,12 +1,19 @@
-import { Response } from 'express';
+import { Response, NextFunction } from 'express';
 import httpStatus from 'http-status';
 import { AuthenticatedRequest } from '@/middlewares';
 import bookingService from '@/services/booking-service';
 
-export async function listBooking(req: AuthenticatedRequest, res: Response) {
-  const { userId } = req;
-  const booking = await bookingService.getBooking(userId);
-  res.status(httpStatus.OK).send(booking);
+export async function listBooking(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+  try {
+    const { userId } = req;
+    const booking = await bookingService.getBooking(userId);
+    return res.status(httpStatus.OK).send({
+      id: booking.id,
+      Room: booking.Room,
+    });
+  } catch (error) {
+    next(error);
+  }
 }
 
 export async function bookingRoom(req: AuthenticatedRequest, res: Response) {
--- src/middlewares/error-handling-middleware.ts
+++ src/middlewares/error-handling-middleware.ts
@@ -1,8 +1,13 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
 import { ApplicationError } from '@/protocols';
 
-export function handleApplicationErrors(err: ApplicationError | Error, _req: Request, res: Response) {
+export function handleApplicationErrors(
+  err: ApplicationError | Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) {
   if (err.name === 'CannotEnrollBeforeStartDateError') {
     return res.status(httpStatus.BAD_REQUEST).send({
       message: err.message,
@@ -32,11 +37,25 @@ export function handleApplicationErrors(err: ApplicationError | Error, _req: Req
       message: err.message,
     });
   }
+
+  if (err.name === 'CannotListHotelsError') {
+    return res.status(httpStatus.NOT_FOUND).send({
+      message: err.message,
+    });
+  }
+
+  if (err.name === 'BadRequestError') {
+    return res.status(httpStatus.BAD_REQUEST).send({
+      message: err.message,
+    });
+  }
+
   if (err.name === 'ForBiddenError') {
     return res.status(httpStatus.FORBIDDEN).send({
       message: err.message,
     });
   }
+
   if (err.name === 'CannotBookingError') {
     return res.status(httpStatus.FORBIDDEN).send({
       message: err.message,
